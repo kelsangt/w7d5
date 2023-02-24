ac42601ad76ddf8b2545438fbe231ccb0c5bb93d
luckyleck <luckyleck29@gmail.com>
2023-02-24 11:18:52 -0500

    Applications Controller

--- reddit_clone/app/controllers/sessions_controller.rb
+++ reddit_clone/app/controllers/sessions_controller.rb
@@ -1,34 +1,5 @@
 class SessionsController < ApplicationController
 
-    before_action :require_logged_in
-
-    helper_methods :current_user, :logged_in?
-
-    def current_user
-        @current_user ||= User.find_by(session_token: session[:session_token])
-    end
-
-    def require_logged_in
-
-        if session[:session_token] = current_user.session_token
-
-    end
-
-    def logged_in?
-        !!current_user
-    end
-
-    def login(user)
-        self.session_token = user.reset_session_token!
-    end
-
-    def logout(user)
-        current_user.reset_session_token! if logged_in? 
-        session[:session_token] = nil
-        @current_user = nil
-    end
-
-
     
 
 end
